6e6ed96877d31bf3275bd9adf4ad0973ec2c538b
ling <704733096@qq.com>
2020-07-28 17:06:06 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>home
+    <el-button type="info" @click="edit">退出</el-button></div>
+</template>
+
+<script>
+export default {
+    methods:{
+        edit:function(){
+            window.sessionStorage.clear();
+            this.$router.push('/login');
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
+
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Input, Form, FormItem, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Input)
+Vue.use(Form)
+Vue.use(FormItem)
+    // 把弹窗组件挂载到全局
+Vue.prototype.$message = Message
