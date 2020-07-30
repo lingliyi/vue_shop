726d16ed14343c20b3d954765742ae97d6ff1d07
ling <704733096@qq.com>
2020-07-30 17:36:31 +0800

    完成用户列表功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,32 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 
 Vue.use(VueRouter)
 
 
 
 const router = new VueRouter({
-        routes: [
-            //路由重定向
-            { path: '/', redirect: '/login' },
-            { path: '/login', component: Login },
-            { path: '/home', component: Home },
-        ]
-    })
-    //挂载路由守卫
+    routes: [
+        //路由重定向
+        { path: '/', redirect: '/login' },
+        { path: '/login', component: Login },
+        {
+            path: '/home',
+            component: Home,
+            redirect: '/welcome',
+            children: [
+                { path: '/welcome', component: Welcome },
+                { path: '/users', component: Users }
+            ]
+        },
+    ]
+})
+
+
+//挂载路由守卫
 router.beforeEach((to, from, next) => {
     //to 将要访问的路径  from 代表从哪个路径来 next 是一个函数表示放行
     // next() 放行    next('/login') 强制跳转 /login
